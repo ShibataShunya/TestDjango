501562b1fbf69b12407032cdc78a8e2899ace7df
NUCBOXM3\shibata <cm24022@g.kogakuin.jp>
2024-08-03 14:16:14 +0900

    ジェネリックビューを使用したテーブルの作成

--- testDjango/hello/urls.py
+++ testDjango/hello/urls.py
@@ -1,6 +1,10 @@
 from django.urls  import path
 from .views import HelloView
+from .views import FriendList
+from .views import FriendDetail
 
 urlpatterns = [
     path('', HelloView.as_view(), name = 'index'),
+    path("list", FriendList.as_view()),
+    path("detail/<int:pk>", FriendDetail.as_view()),
 ]
--- testDjango/hello/views.py
+++ testDjango/hello/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import TemplateView
 from .models import Friend
+from django.views.generic import ListView
+from django.views.generic import DetailView
 # from .forms import HelloForm
 # from .forms import Sessionform
 
@@ -17,3 +19,9 @@ class HelloView(TemplateView):
 
     def get(self, request: HttpRequest):
         return render(request, 'hello/index.html', self.params)
+    
+class FriendList(ListView):
+    model = Friend
+
+class FriendDetail(DetailView):
+    model = Friend
